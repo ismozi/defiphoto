b72f770efee9c8c29a3c8f26758618095779faf4
ismozi <61505183+ismozi@users.noreply.github.com>
2020-03-26 15:00:08 -0400

    Modifications mineures

--- lib/App.dart
+++ lib/App.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
 import 'screens/login.dart';
+import 'screens/progression.dart';
+
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
    debugShowCheckedModeBanner: false,
+  
+  routes: <String, WidgetBuilder> {
+    '/screen1': (BuildContext context) => new Login(),
+    '/screen2' : (BuildContext context) => new MainPage(),
+    '/screen3' : (BuildContext context) => new Progression()
+  },
 
       theme: new ThemeData(
         primarySwatch: Colors.cyan,
--- lib/screens/listViewWidget.dart
+++ lib/screens/listViewWidget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'pageCommentaire.dart';
-import 'fadeRoute.dart';
+
 
 class listViewWidget extends StatefulWidget {
   final int nbItem;
