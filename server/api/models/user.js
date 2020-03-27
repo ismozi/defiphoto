b27663d0bc559cadb1aee9026bfdecd29d3adebf
Rony <ronymawad@gmail.com>
2020-03-27 19:24:55 -0400

    ajouter stage à la BD

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -8,10 +8,14 @@ const userSchema = mongoose.Schema({
     email : {type : String, required : true, match : /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
     password : {type : String, required : true},
     role :{type : String, required : true},
-    createdAt : mongoose.Schema.Types.Date,
-    schoolYearBegin :{type : String, required : false},
-    schoolYearEnd : {type : String, required : false},
-    //internships
+    // createdAt : mongoose.Schema.Types.Date,
+    schoolYearBegin :{type : Date, required : false},
+    schoolYearEnd : {type : Date, required : false},
+    stageName : {type : String, required : true},
+    stageDesc : {type : String, required : true},
+    stageBegin : {type : Date, required : false},
+    stageEnd : {type : Date, required : false},
+    
 
     //could add date and time
 });
