1396ae3283df671e4c3e3956a82a2928543f68d6
madzou64 <61238940+madzou64@users.noreply.github.com>
2020-04-21 19:13:28 -0400

    Add files via upload
    Deuxieme version des autre pages principal de la vue prof

---
+++ lib/screens/searchPageV2.dart
@@ -0,0 +1,162 @@
+
+
+
+import 'package:flutter/material.dart';
+
+import 'modelVueProf.dart';
+import 'pageEtudiantV2.dart';
+
+class SearchPage extends SearchDelegate<Etudiant> {
+  List<Etudiant> listEtudiant;
+  List<Etudiant> recentEtudiant;
+  int indexEtudiants;
+  bool found = false;
+  String hintText;
+
+  SearchPage({this.listEtudiant, this.recentEtudiant, this.hintText})
+      : super(
+    searchFieldLabel: hintText,
+    keyboardType: TextInputType.text,
+    textInputAction: TextInputAction.search,
+    //  todo transitionAnimation
+  );
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return [
+      IconButton(
+        icon: Icon(
+          Icons.clear,
+        ),
+        onPressed: () {
+          query = '';
+          showSuggestions(context);
+        },
+        tooltip: 'Annuler la recherche',
+      ),
+      IconButton(
+        icon: Icon(Icons.delete),
+        onPressed: () {
+          recentEtudiant.clear();
+        },
+        tooltip: 'Supprimer les recherche recentes',
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        if (found == true) {
+          showSuggestions(context);
+        } else {
+          close(context, null);
+        }
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    return PageEtudiants(etudiant: listEtudiant[indexEtudiants]);
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions changer la facon dont l
+
+    found = false;
+    List<Etudiant> suggestionList = query.isEmpty
+        ? recentEtudiant
+        : listEtudiant.where((p) => p.nomComplet.startsWith(query)).toList();
+    String textInfo = query.isEmpty
+        ? 'Recherche récente'
+        : 'Resultat( ${suggestionList.length} )';
+
+    return ListView(
+      children: <Widget>[
+        SizedBox(height: 5,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(width: 10,),
+            Text(textInfo,style: TextStyle(fontSize: 15,fontStyle: FontStyle.italic),),
+          ],
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.85,
+          child: suggestionList.isEmpty && query.isNotEmpty?Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: MediaQuery.of(context).size.height*0.30,),
+                Icon(Icons.search,size: 200,),
+                Text('Aucun résulat correspondant à cette recherche:" $query "  ')
+              ],
+            ),
+          ):ListView.separated(
+            separatorBuilder: (context, index) => Divider(),
+            itemBuilder: (context, index) => ListTile(
+              trailing: Icon(Icons.arrow_forward),
+              leading: query.isEmpty ? Icon(Icons.restore) : Icon(Icons.person),
+              subtitle: Text('Id Élève : ${suggestionList[index].idDonne}'),
+              title: RichText(
+                strutStyle: StrutStyle(fontSize: 19),
+                text: TextSpan(
+                    text: suggestionList[index]
+                        .nomComplet
+                        .substring(0, query.length),
+                    style: TextStyle(
+                      //color: Colors.black,
+                      fontWeight: FontWeight.w200,
+                    ),
+                    children: [
+                      TextSpan(
+                          text: suggestionList[index]
+                              .nomComplet
+                              .substring(query.length),
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                          ))
+                    ]),
+              ),
+              onTap: () {
+                int cpt = 0;
+                for (int i = 0; i < recentEtudiant.length; i++) {
+                  if (suggestionList[index].idSearch ==
+                      recentEtudiant[i].idSearch) {
+                    cpt++;
+                  }
+                }
+                if (cpt == 0) {
+                  recentEtudiant.add(suggestionList[index]);
+                }
+                indexEtudiants = suggestionList[index].idSearch;
+                found = true;
+                showResults(context);
+              },
+            ),
+            itemCount: suggestionList.length,
+            padding: EdgeInsets.all(2),
+          ),
+        ),
+      ],
+    );
+  }
+
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    // TODO: implement appBarTheme
+    return ThemeData(
+      brightness: Brightness.dark,
+    );
+  }
+}
